2a9573832111947a04b5c8550b1bc868c70ab160
Kang Hyun Woo <gusdn741021@gmail.com>
2024-10-10 10:30:48 +0900

    first commit

---
+++ src/main/java/com/lunch/backend/LunchBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lunch.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LunchBackendApplication.class, args);
+	}
+
+}
